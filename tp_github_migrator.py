28c83d79f8454b781c68082d85c0c9cae9b3c6dd
Ganeshwara Herawan Hananda <soulmagic13@gmail.com>
2018-09-17 16:37:51 +0100

    fetch TP comments

--- tp_github_migrator.py
+++ tp_github_migrator.py
@@ -1,10 +1,29 @@
 import github as gh
 import csv
-
+import requests
+import json
 
 # https://developer.github.com/v3/issues/#create-an-issue
 
 #### helpers ####
+def get_github_comments_from_tp_requirement(tp_requirement_id):
+    tp_url = "https://work.grakn.ai/api/v1/assignables/{}/comments".format(tp_requirement_id)
+    tp_querystring = {"format": "json"}
+    tp_headers = {
+        'Authorization': "Basic Z2FuZXNoOkRlNXYzcjFncg==",
+        'Cache-Control': "no-cache",
+        'Postman-Token': "5d9693d3-0177-405f-8080-ccbad8663b9f"
+    }
+    tp_response = requests.request("GET", tp_url, headers=tp_headers, params=tp_querystring)
+    tp_response_json = json.loads(tp_response.text)
+
+    github_comments = []
+    for tp_comment in tp_response_json['Items']:
+        tp_original_poster = tp_comment['Owner']['FirstName'] + ' ' + tp_comment['Owner']['LastName']
+        gh_comment = '~This comment was originally posted by {} on {}~.\n\n{}'.format(tp_original_poster, tp_comment['CreateDate'], tp_comment['Description'])
+        github_comments.append(gh_comment)
+    return github_comments
+
 def tp_requirements_to_github_issues(tp_requirements):
     def tp_users_to_github_users(tp_users):
         github_users = []
@@ -57,6 +76,7 @@ def tp_requirements_to_github_issues(tp_requirements):
             'body': tp_req['Description'].replace('\n', '\n\n'),
             'labels': gh_project + gh_request_type + gh_state,
             'assignees': tp_users_to_github_users(tp_req['Assignments']),
+            'comments': get_github_comments_from_tp_requirement(tp_req['ID'])
         }
 
         github_issues.append(gh_issue)
@@ -67,6 +87,9 @@ def tp_requirements_to_github_issues(tp_requirements):
 #### main ####
 # details of the migration source
 tp_requirement_csv_path = './data/tp/Requirements Export - Edit v2.csv'
+tp_user = 'ganesh'
+tp_password = 'CPelZ1iGaUNPqMII7rGr'
+
 # details of the migration target
 github_user = 'lolski'
 github_password = 'p4UbNNQO5ToEBDDJvptO'
@@ -84,7 +107,9 @@ if __name__ == '__main__':
     for issue in github_issues:
         key = issue['repository']
         target_repo = github_organisation.get_repo(key)
-        print('repository = {}, title = {}, body = {}, labels = {}, assignees = {}'.format(key, issue['title'][0:20] + '...', issue['body'][0:20] + '...', issue['labels'], issue['assignees']))
+        print('repository = {}, title = {}..., body = {}..., labels = {}, assignees = {}'.format(key, issue['title'][0:20], issue['body'][0:20], issue['labels'], issue['assignees']))
+        for gh_comment in issue['comments']:
+            print(' - comment: ...{}...'.format(gh_comment.replace('\n', '')[100:240]))
         # target_repo.create_issue(title=issue['title'], body=issue['body'], labels=issue['labels'], assignees=issue['assignees']) # TODO: repository
 
     print('done.')
