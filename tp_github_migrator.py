66a49ca8a2f6e9e814dcfd8c3dc46e8391e98dd5
Ganeshwara Herawan Hananda <soulmagic13@gmail.com>
2018-09-17 11:21:32 +0100

    add user mapping. TODO: repository mapping

--- tp_github_migrator.py
+++ tp_github_migrator.py
@@ -6,20 +6,51 @@ import csv
 
 #### helpers ####
 def tp_requirements_to_github_issues(tp_requirements):
+    def tp_users_to_github_users(tp_users):
+        github_users = []
+        map = {
+            '': '',
+            'Ganeshwara Hananda': 'lolski',
+            'Haikal Pribadi': 'haikalpribadi',
+            'Marco Scoppetta': 'marco-scoppetta',
+            'Kasper Piskorski': 'kasper-piskorski',
+            'Syed Irtaza Raza': 'Irtazaraza',
+            'Joshua Send': 'flyingsilverfin',
+            'Jyothish Soman': 'jyosoman',
+            'Soroush Saffari': 'sorsaffari',
+            'James Fletcher': 'jmsfltchr',
+            'Tomas Sabat': 'tomassabat',
+        }
+
+        tp_users_as_list = [] if tp_users == '' else tp_users.split(', ')
+        for tp_user in tp_users_as_list:
+            key = tp_user.replace('(Developer) ', '')
+            github_user = map[key]
+            github_users.append(github_user)
+
+        return github_users
+
     github_issues = []
     for req in tp_requirements:
+        repository = 'grakn-kgms' if req['Project'] == 'Grakn KGMS' else 'grakn-core'
+        project = [req['Project'].lower()]
+        request_type = [req['Request Type'].lower()]
+        state = ['in progress'] if req['Entity State'] == 'In Progress' else []
+
+        issue = {
+            'repository': repository,
+            'title': req['Name'],
+            'body': req['Description'].replace('\n', '\n\n'),
+            'labels': project + request_type + state,
+            'assignees': tp_users_to_github_users(req['Assignments']),
+        }
+
         print("=====")
         print(req['Description'])
         print("=====")
-        issue = {
-            'repository': 'grakn-kgms' if req['Project'] == 'Grakn KGMS' else 'grakn-core',                         # grakn-core if project != 'Grakn KGMS', grakn-kgms otherwise
-            'title': req['Name'],                                                                                   # name
-            'body': req['Description'].replace('\n', '\n\n'),                                                                             # description
-            'labels': [req['Project'], req['Request Type']] + (['in progress'] if req['Entity State'] == 'In Progress' else []),    # request type + state
-            'assignees': ['lolski', 'haikalpribadi'],                                                                      # assignment TODO: parse and map
-        }
-        print("===")
         print(issue)
+        print("===")
+
         github_issues.append(issue)
 
     return github_issues
@@ -27,20 +58,21 @@ def tp_requirements_to_github_issues(tp_requirements):
 
 #### main ####
 # details of the migration source
-tp_requirement_csv_path = '/Users/lolski/Playground/tp_github/data/tp/Requirements Export.csv'
+tp_requirement_csv_path = './data/tp/Requirements Export - Edit v2.csv'
 # details of the migration target
 github_user = 'lolski'
 github_password = 'p4UbNNQO5ToEBDDJvptO'
 github_organisation_name = 'graknlabs'
-github_repository_name = 'fake-grakn'
+github_grakn_core_repository_name = 'fake-grakn'
+github_grakn_kgms_repository_name = 'fake-grakn-kgms' # TODO: use
 
 if __name__ == '__main__':
-    tp_requirements = list(csv.DictReader(open(tp_requirement_csv_path)))[0:1]
+    tp_requirements = list(csv.DictReader(open(tp_requirement_csv_path)))[0:3]
     github_issues = tp_requirements_to_github_issues(tp_requirements)
 
     print(github_issues)
     github_connection = gh.Github(github_user, github_password)
     github_organisation = github_connection.get_organization(github_organisation_name)
-    github_repository = github_organisation.get_repo(github_repository_name)
+    github_repository = github_organisation.get_repo(github_grakn_core_repository_name)
     for issue in github_issues:
-        github_repository.create_issue(title=issue['title'], body=issue['body'], labels=issue['labels'], assignees=issue['assignees'])
+        github_repository.create_issue(title=issue['title'], body=issue['body'], labels=issue['labels'], assignees=issue['assignees']) # TODO: repository
